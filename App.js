1f09e2b485099604ab29e4957e7156d8a7ef1d9d
atimilson <atimilson95@gmail.com>
2022-01-27 02:02:15 -0400

    final aula 1

--- App.js
+++ App.js
@@ -1,62 +1,107 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, TextInput } from 'react-native';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
 import Picker from './src/components/Picker'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <StatusBar translucent={false} backgroundColor='#efd'/>
+ 
       <View style={styles.areaMoeda}>
-         <Text style={styles.titulo}>
-           ADICIONE UMA MOEDA
-         </Text>
-         <Picker />
-
+       <Text style={styles.titulo}>Selecione sua moeda</Text>
+       <Picker/>
       </View>
-      <View style={[styles.areaValor,styles.areaMoeda]}>
-        <Text style={styles.titulo}>Digite um valor para converter em (R$)</Text>  
-        <TextInput
-        placeholder="EX: 150" 
-        style={styles.input}
-
-        />
-      </View> 
-
+ 
+      <View style={styles.areaValor}>
+       <Text style={styles.titulo}>Digite um valor para converter em (R$)</Text>
+       <TextInput
+       placeholder="EX: 150"
+       style={styles.input}
+       keyboardType="numeric"
+       />
+      </View>
+ 
+     <TouchableOpacity style={styles.botaoArea}>
+       <Text style={styles.botaoTexto}>Converter</Text>
+     </TouchableOpacity>
+ 
+     <View style={styles.areaResultado}>
+       <Text style={styles.valorConvertido}>
+         3 USD
+       </Text>
+       <Text style={[styles.valorConvertido, { fontSize: 18, margin: 10 } ]}>
+         Corresponde a
+       </Text>
+       <Text style={styles.valorConvertido}>
+         19,90
+       </Text>
+     </View>
+ 
     </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#101215',
-    alignItems: 'center',
-    justifyContent: 'flex-start',
-    paddingTop : 40,
-  },
-  areaMoeda:{
+   );
+ }
+ 
+ const styles = StyleSheet.create({
+   container:{
+     flex:1,
+     alignItems: 'center',
+     backgroundColor: '#101215',
+     paddingTop: 60
+   },
+   areaMoeda:{
+     width: '90%',
+     backgroundColor: '#F9f9f9',
+     paddingTop: 9,
+     paddingBottom: 9,
+     borderTopLeftRadius: 9,
+     borderTopRightRadius: 9,
+     marginBottom: 2,
+   },
+   titulo:{
+     fontSize: 15,
+     color: '#000',
+     paddingTop: 5,
+     paddingLeft: 5,
+   },
+   areaValor:{
+     width: '90%',
+     backgroundColor: '#F9f9f9',
+     paddingBottom: 9,
+     paddingTop: 9
+   },
+   input:{
+     width: '100%',
+     padding: 10,
+     height: 45,
+     fontSize: 20,
+     marginTop: 8,
+     color: '#000'
+   },
+   botaoArea:{
     width: '90%',
-    backgroundColor: '#f9f9f9',
-    paddingTop:9,
-    borderTopRightRadius: 9,
-    borderTopLeftRadius: 9,
-    marginBottom: 1,
-
-  },
-  titulo:{
-    fontSize: 16,
-    color: '#000',
-    paddingTop:5,
-    paddingLeft:5,
-    marginBottom: 10, 
-  },
-  input:{
-    width: '100%',
-    padding: 10,
-    height: 46,
-    fontSize: 18,
-    color: '#000',
-  }
-
-});
+    backgroundColor: '#FB4b57',
+    height: 45 ,
+    borderBottomLeftRadius: 9,
+    borderBottomRightRadius: 9,
+    justifyContent: 'center',
+    alignItems: 'center'
+   },
+   botaoTexto:{
+     fontSize: 18,
+     color:'#FFF',
+     fontWeight: 'bold'
+   },
+   areaResultado:{
+     width: '90%',
+     backgroundColor:'#FFF',
+     marginTop: 35,
+     alignItems: 'center',
+     justifyContent: 'center',
+     padding: 25
+   },
+   valorConvertido:{
+     fontSize: 39,
+     fontWeight: 'bold',
+     color: '#000'
+   }
+ });  
